14386509760c5c02389c59fe6c14f5fa9bc658f2
Urameshi53 <zigahemmanuel53@gmail.com>
2026-02-27 18:09:47 +0000

    Avatar changes

--- finflow/finflow/accounts/serializers.py
+++ finflow/finflow/accounts/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import Avatar
 from django.contrib.auth.models import User 
+from requests import Response
+from http import HTTPStatus as status
 
 class AvatarSerializer(serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
@@ -9,3 +11,11 @@ class AvatarSerializer(serializers.ModelSerializer):
         model = Avatar
         fields = ['id', 'user', 'currency']
         read_only_fields = ['id']
+        
+    def put(self, request, pk):
+        avatar = Avatar.objects.get(pk=pk)
+        serializer = AvatarSerializer(avatar, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
